c98dcf3690b9a9d641a01040a97052935e5b318f
d-kang <david.kang714@gmail.com>
2017-11-18 10:36:50 -0800

    Init Fetch And Youtube Node API and Set up Keys

--- server/index.js
+++ server/index.js
@@ -1,21 +1,33 @@
 /**
  * @Date:   11.13.2017 07:23pm
  * @Filename: index.js
- * @Last modified time: 11.18.2017 10:24am
+ * @Last modified time: 11.18.2017 10:36am
  */
 
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import cors from 'cors';
+import morgan from 'morgan';
+import fetch from 'node-fetch';
+import YouTube from 'youtube-node';
+import { YOUTUBE_API_KEY, CLIENT_ID } from './config/youtube-api';
 
 const app = express();
 const port = process.env.PORT || 3500;
+const youTube = new YouTube();
+
+youTube.setKey(YOUTUBE_API_KEY)
+
+app.use(cors());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
 app.get('/api/youtube', (req, res) => {
-  cosnole.log('hi from api youtube');
+  fetch('')
+  res.send('hi');
 })
 
 app.get('/*', (req, res) => {
