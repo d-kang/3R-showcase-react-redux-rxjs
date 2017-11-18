dd61484de6d1f5a7bdf2953dbc8ea2f150449294
d-kang <david.kang714@gmail.com>
2017-11-18 11:13:22 -0800

    Set State for Payload to send to server

--- src/components/YoutubeSearch.jsx
+++ src/components/YoutubeSearch.jsx
@@ -1,26 +1,37 @@
 /**
  * @Date:   11.18.2017 10:02am
  * @Filename: YoutubeSearch.jsx
- * @Last modified time: 11.18.2017 10:57am
+ * @Last modified time: 11.18.2017 11:13am
  */
 import React, { Component } from 'react';
 
 class YoutubeSearch extends Component {
+  state = {
+    payload: '',
+  }
   handleSubmit = (e) => {
     e.preventDefault();
-    fetch('http://localhost:3500/api/youtube')
+    const { payload } = this.state;
+    console.log('payload', payload);
+    fetch('http://localhost:3500/api/youtube', { payload })
       .then((res) => res.json())
-      .then((res) => console.log('res', res))
+      .then((res) => console.log('res>>>', res))
       .catch((err) => console.log('err>>>', err));
+  }
+  handleInput = (e) => {
+    const payload = e.target.value;
+    this.setState({ payload });
   }
   render() {
     return (
       <div>
         <form
           onSubmit={this.handleSubmit}
-          action=""
         >
-          <input type="text"/>
+          <input
+            type="text"
+            onChange={this.handleInput}
+          />
           <button>Search Youtube</button>
         </form>
       </div>
