a9c35d2a9c3f3d54c78229eae3a6ff96d0c09395
d-kang <david.kang714@gmail.com>
2017-11-19 18:08:46 -0800

    Display Mock Data Snippet URL to Page

--- src/components/YoutubeSearch.jsx
+++ src/components/YoutubeSearch.jsx
@@ -4,10 +4,11 @@
  * @Last modified time: 11.18.2017 11:48am
  */
 import React, { Component } from 'react';
-
+import youTubeSampleData from '../data/data.json';
 class YoutubeSearch extends Component {
   state = {
     payload: '',
+    response: youTubeSampleData,
   }
 
   headers = {
@@ -33,6 +34,7 @@ class YoutubeSearch extends Component {
     this.setState({ payload });
   }
   render() {
+    console.log('response', this.state.response);
     return (
       <div>
         <form
@@ -44,10 +46,57 @@ class YoutubeSearch extends Component {
           />
           <button>Search Youtube</button>
         </form>
-        <iframe width="420" height="345" src="http://www.youtube.com/embed/AQBh9soLSkI?autoplay=1" frameborder="0" allowfullscreen></iframe>
+        <iframe width="420" height="345" src="http://www.youtube.com/embed/AQBh9soLSkI?autoplay=1" frameBorder="0" allowFullScreen></iframe>
+
+        <div>
+          {
+            this.state.response.items.map(({snippet}, index) => {
+              return (
+                <div>
+                  <img src={snippet.thumbnails.medium.url} alt=""/>
+                </div>
+              )
+            })
+          }
+        </div>
+
       </div>
     );
   }
 }
+
+// {
+//   "kind": "youtube#searchResult",
+//   "etag": "ld9biNPKjAjgjV7EZ4EKeEGrhao/cQcWyA3ZtzMxISquwIpGbyAn-io",
+//   "id": {
+//     "kind": "youtube#video",
+//     "videoId": "SAvXjWgKSQ8"
+//   },
+//   "snippet": {
+//     "publishedAt": "2017-11-18T16:34:37.000Z",
+//     "channelId": "UCSJ4gkVC6NrvII8umztf0Ow",
+//     "title": "lofi hip hop radio - beats to relax/study to",
+//     "description": "Thank you for listening, I hope you will have a good time here :) ▭▭▭▭▭▭▭▭▭▭▭▭▭▭▭▭▭▭▭▭▭▭ ▸Update: -Day mode will be available soon ...",
+//     "thumbnails": {
+//       "default": {
+//         "url": "https://i.ytimg.com/vi/SAvXjWgKSQ8/default_live.jpg",
+//         "width": 120,
+//         "height": 90
+//       },
+//       "medium": {
+//         "url": "https://i.ytimg.com/vi/SAvXjWgKSQ8/mqdefault_live.jpg",
+//         "width": 320,
+//         "height": 180
+//       },
+//       "high": {
+//         "url": "https://i.ytimg.com/vi/SAvXjWgKSQ8/hqdefault_live.jpg",
+//         "width": 480,
+//         "height": 360
+//       }
+//     },
+//     "channelTitle": "ChilledCow",
+//     "liveBroadcastContent": "live"
+//   }
+// }
 
 export default YoutubeSearch;
