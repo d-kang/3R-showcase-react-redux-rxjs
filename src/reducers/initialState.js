c39983166598202ea76434ffc1126c14d7e20181
d-kang <david.kang714@gmail.com>
2017-11-22 10:58:53 -0800

    Move Initial State File

---
+++ src/reducers/initialState.js
@@ -0,0 +1,28 @@
+import youTubeSampleData from '../data/data.json';
+
+const initialState = {
+  ping: {
+    isPinging: false,
+  },
+  beep: {
+    isBeeping: false,
+    someArr: [1, 2, 3, 4, 5],
+  },
+  githubUser: {
+    isLoading: false,
+    value: '',
+    fetchUserResponse: [],
+  },
+  githubRepos: {
+    isLoading: false,
+    value: '',
+    fetchRepoResponse: [],
+  },
+  youtube: {
+    isLoading: false,
+    value: '',
+    fetchYoutubeResponse: [...youTubeSampleData.items],
+  },
+};
+
+export default initialState;
