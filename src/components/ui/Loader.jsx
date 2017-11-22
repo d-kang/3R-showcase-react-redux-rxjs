3e76b7be02785d4810fc7e2d2f3025311d07d1ba
d-kang <david.kang714@gmail.com>
2017-11-22 13:00:37 -0800

    Add Loader Component

---
+++ src/components/ui/Loader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { CircularProgress, MuiThemeProvider } from 'material-ui';
+
+export default () => (
+  <MuiThemeProvider>
+    <CircularProgress />
+  </MuiThemeProvider>
+)
