13e3581111a98098a054711ec1866f816e5169fb
d-kang <david.kang714@gmail.com>
2017-11-22 13:00:12 -0800

    Add Video List Component

---
+++ src/components/youtube/VideoList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default ({ response, setCurrentVideo }) => (
+  response.map(({ id, snippet }, i) => (
+    <div
+      key={i}
+    >
+      <img
+        alt={snippet.description}
+        onClick={() => setCurrentVideo(id.videoId)}
+        src={snippet.thumbnails.medium.url}
+      />
+      <div>{snippet.title}</div>
+    </div>
+  ))
+)
