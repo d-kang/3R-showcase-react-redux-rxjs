9762d0fa40fe73835706eb667422850c61a3fa66
d-kang <david.kang714@gmail.com>
2017-11-29 12:28:13 -0800

    Set Types for Props

--- src/components/youtube/VideoList.tsx
+++ src/components/youtube/VideoList.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import VideoItem from './VideoItem';
-export default ({ response, setCurrentVideo, currentVideoId, setCurrentKey, currentKey }) => (
+export default ({ response, setCurrentVideo, currentVideoId, setCurrentKey, currentKey } : { response: object[], setCurrentVideo: void, currentVideoId: string, setCurrentKey: void, currentKey: void  }) => (
   response.map((props, i) => (
     <VideoItem
       key={i}
--- src/components/youtube/VideoPlayer.tsx
+++ src/components/youtube/VideoPlayer.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export default ({ currentVideo }) => (
+export default ({ currentVideo } : { currentVideo: string }) => (
   <iframe
     width="854"
     height="510"
