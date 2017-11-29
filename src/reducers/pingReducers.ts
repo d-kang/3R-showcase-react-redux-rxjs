1aac48bbd4304a75d7935ebb7537dcb1bcf8d0db
d-kang <david.kang714@gmail.com>
2017-11-29 12:37:46 -0800

    Example Of How To Type An Array In An Object Literal for Reducers

--- src/reducers/pingReducers.ts
+++ src/reducers/pingReducers.ts
@@ -1,7 +1,7 @@
 import * as types from '../actions/actionTypes';
 import { initialState } from './initialState';
 
-export const pingReducer = (state = initialState.ping, action: object) => {
+export const pingReducer = (state = initialState.ping, action: { type: string}) => {
   switch (action.type) {
     case types.PING:
       return { isPinging: true };
@@ -12,15 +12,21 @@ export const pingReducer = (state = initialState.ping, action: object) => {
   }
 };
 
+interface Beepsss {
+  isBeeping: boolean,
+  someArr: any[],
+  foo: string,
+}
+const myNextStateExample: Beepsss = {
+  isBeeping: true,
+  someArr: [],
+  foo: 'ooooo',
+}
 
-export const beepReducer = (state = initialState.beep, action: object) => {
+export const beepReducer = (state = initialState.beep, action: { type: string, foo: number[] }) => {
   switch (action.type) {
     case types.BEEP:
-      return {
-        isBeeping: true,
-        someArr: [],
-        foo: 'ooooo',
-      };
+      return myNextStateExample;
     case types.BOOP:
       return {
         isBeeping: false,
