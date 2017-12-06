ab0bef61afd264c9992ca720a64d90b36efe6c68
d-kang <david.kang714@gmail.com>
2017-12-06 15:57:59 -0800

    Add BlockChain Component

---
+++ src/components/blockChain/BlockChain.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class BlockChain extends React.Component {
+  // finder = () => {
+  //
+  // }
+  render() {
+    return (
+      <div>{'hello world'}</div>
+    );
+  }
+
+}
+
+export default BlockChain;
