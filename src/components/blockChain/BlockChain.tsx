3a4e8566448a30f5f142d7e92d804a24091952f6
d-kang <david.kang714@gmail.com>
2017-12-06 18:51:04 -0800

    Add A Calculate Profit Feature

--- src/components/blockChain/BlockChain.tsx
+++ src/components/blockChain/BlockChain.tsx
@@ -3,6 +3,9 @@ import * as React from 'react';
 class BlockChain extends React.Component {
   state = {
     data: {},
+    holding: 0,
+    purchasedPrice: 0,
+    gains: 0,
   }
   componentDidMount() {
     this.fetchExchangeRates()
@@ -19,8 +22,25 @@ class BlockChain extends React.Component {
       this.fetchExchangeRates()
     },15000);
   }
+  totalBalance = () => {
+    return this.state.data.USD ? this.state.holding * this.state.data['USD'].buy : 0
+  }
+  handleHoldingSubmit = (e) => {
+    e.preventDefault();
+    const holding = Number(e.target[0].value)
+    this.setState({holding});
+  }
+  handlePurchasedPriceSubmit = (e) => {
+    e.preventDefault();
+    const purchasedPrice = Number(e.target[0].value)
+    console.log('purchasedPrice', purchasedPrice);
+    console.log('typeof purchasedPrice', typeof purchasedPrice);
+    console.log('this.state.holding', this.state.holding);
+    console.log('typeof this.state.holding', typeof this.state.holding);
+    this.setState({gains: this.totalBalance() - purchasedPrice});
+  }
   render() {
-    const { USD } = this.state.data
+    const { data } = this.state
     return (
       <div>
         <h1>BlockChain</h1>
@@ -28,8 +48,33 @@ class BlockChain extends React.Component {
           onClick={this.timer}
         >Update Rates Every Second</button>
 
-        <div>BITCOIN PRICE: ${USD && USD.buy}</div>
+        <div>BITCOIN PRICE: ${data && data.USD && data.USD.buy}</div>
 
+        <form
+          action=""
+          onSubmit={this.handleHoldingSubmit}
+        >
+          <input
+            type="text"
+            placeholder="Holding"
+          />
+          <button>enter</button>
+        </form>
+
+
+        <form
+          action=""
+          onSubmit={this.handlePurchasedPriceSubmit}
+        >
+          <input
+            type="text"
+            placeholder="purchased at"
+          />
+          <button>enter</button>
+        </form>
+
+        <div>total balance: {this.totalBalance()}</div>
+        <div>gains: {this.state.gains}</div>
       </div>
     );
   }
