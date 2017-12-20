3a3a2fdde61e2ebdc1d749675c824326ddadfe6e
d-kang <david.kang714@gmail.com>
2017-12-20 13:57:49 -0800

    Revert Youtube Video Item to Not Use Card

--- src/components/youtube/VideoItem.jsx
+++ src/components/youtube/VideoItem.jsx
@@ -18,16 +18,14 @@ class VideoItem extends React.Component {
     const { snippet, id, setCurrentVideo, myKey, currentKey } = this.props;
     return (
       <div>
-      <Card
-        title={snippet.title}
-        subtitle={snippet.channelTitle}
-        img={snippet.thumbnails.medium.url}
-        description={snippet.description}
-        setToPlay={this.setToPlay}
-        action1={'Play'}
-      />
-        { myKey === currentKey && <div>Now Playing</div>}
-      </div>
+       <img
+         alt={snippet.description}
+         onClick={this.setToPlay}
+         src={snippet.thumbnails.medium.url}
+       />
+       { myKey === currentKey && <div>Now Playing</div>}
+       <div>{snippet.title}</div>
+     </div>
     )
   }
 }
